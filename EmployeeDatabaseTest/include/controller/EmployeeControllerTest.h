2450721c77c67f4335f6b6be6237f2f4e4ab4756
Rushi Gandhi <rushigandhi1109@gmail.com>
2024-03-31 02:13:39 +0530

    Testing Employee Controller class

---
+++ EmployeeDatabaseTest/include/controller/EmployeeControllerTest.h
@@ -0,0 +1,15 @@
+#ifndef __EmployeeControllerTest_h__
+#define __EmployeeControllerTest_h__
+
+#include "../../include/controller/EmployeeController.h"
+
+using EmployeeDB::Controller::EmployeeController;
+
+class EmployeeControllerTest : public EmployeeController {
+public:
+	static std::string getUpdateQueryCondition(Employee& employee) {
+		return EmployeeController::getUpdateQueryCondition(employee);
+	}
+};
+
+#endif // !__EmployeeControllerTest_h__
